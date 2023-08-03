d578c4bb79f8caed065de216c5c3dbf47d371715
SakurajimaMai <110098256+Bearyuc@users.noreply.github.com>
2023-08-03 22:34:27 +0800

    Add files via upload
    第一期配置 后续还有nvim改用lazy

---
+++ rofi/launcher.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+theme="spotlight"
+dir="$HOME/.config/rofi/"
+
+# overwrite colors file
+cat > $dir/colors.rasi <<- EOF
+	/* colors */
+
+	* {
+	  al:  $ALPHA;
+	  bg:  $BG;
+	  se:  $SELECT;
+	  fg:  $FG;
+	  ac:  $ACCENT;
+	}
+EOF
+
+rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$theme"
+
